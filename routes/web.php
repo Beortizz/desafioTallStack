8efe1952610a391d64bcf8a16eb0fa8a69ee2c04
Beortizz <beortizz@gmail.com>
2022-09-27 21:31:17 -0300

    correcao na rota

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
     
 Route::get('/', function () {
-    return view('welcome');
+    return view('player.load-player');
 });
 
 Route::get('/players', function() {
